898e50be8f52791244cd824448a28d37566ed39a
coderscode2 <veda59@gmail.com>
2025-11-19 17:52:20 -0700

    Update CSRPackage.java

--- android/src/main/java/com/ecccsr/CSRPackage.java
+++ android/src/main/java/com/ecccsr/CSRPackage.java
@@ -5,13 +5,16 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class CSRPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-        return Collections.singletonList(new CSRModule(reactContext));
+        List<NativeModule> modules = new ArrayList<>();
+        modules.add(new CSRModule(reactContext));
+        return modules;
     }
 
     @Override
